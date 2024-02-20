a79626846ba8a23cab8463df06229995d882ffa1
Niyati <113116250+Niyati2007@users.noreply.github.com>
2024-02-20 12:21:10 -0500

    Update Muhammad_Yousuf.java

--- FirstGitHub/src/Muhammad_Yousuf.java
+++ FirstGitHub/src/Muhammad_Yousuf.java
@@ -3,6 +3,10 @@ public static void main(String[] args)
 {
     System.out.println("Hello Muhammad");
     System.out.println("Hello Mark");
-    
+
+    for(int i = 0; i < 16; i++)
+    {
+        System.out.println("I'm " + i + "years old.");
+    }
 }
 }
